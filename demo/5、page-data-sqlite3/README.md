3ff0eb32346ecaf9004817b6696165a148cca0dc
soyoumi <soomaoimi@gmail.com>
2024-12-10 16:53:17 +0800

    更新

--- demo/5、page-data-sqlite3/README.md
+++ demo/5、page-data-sqlite3/README.md
@@ -1,20 +1,9 @@
 
 
-## 常见组件嵌套多页面交互
-id:morepages
+## 多页面跳转、传递数据、sqlite3
 
 文本、输入框、图片，混合在界面上。<br/>
-读写改查，带结合sqlite3操作
-
-##### 测试环境范围
-
-≈ Flutter 3.24.5 <br/>
-≈ Dart 3.5.4 <br/>
-≈ gradle 8.6<br/>
-≈ Android SDK version 35.0.0 <br/>
-≈ Java build 21.0.3+-12282718-b509.11 <br/>
-≈ Windows 10 SDK version 10.0.19041.0 <br/>
-≈ ndk 28.0.12674087
+读写改查，可以方便了解组件之间如何传递内容的，带sqlite3操作。
 
 ## yaml文件增添的配置
 
@@ -22,19 +11,16 @@ id:morepages
 
 ## dart导入库
 
-所有导入的库<br/>
-一是默认的 material.dart<br/>
-二是：其它自写的代码页面dart文件。
-
-请注意每个dart顶部可能导入一些库，留意 import xxx；
-
-如 “import 'package:demo3/...” 中的 demo3 为项目名称id，yaml文件中顶部 name: 也是这个。
+material.dart<br/>
+其它自写页面dart文件。
 
 ## 遭遇到的问题障碍
 
 朋友帮忙写这个实例的时候，sqlite3的问题，很不容易解决，弄了好久才解决了问题，这个过程中，有更换了sqlite3的操作代码，然后没安装ndk，进行了安装，后面还是没成功，或是手机问题，后来在电脑上使用模拟器成功了，再换手机上也成功了。
 
 请留意这个带sqlite3操作的，我这里是改了ndk版本的设置，因为出现了ndk版本不适配的问题。
+
+如果编译出现错误，尝试按照以下更改，ndk版本要改成你那里的版本，怎么查看ndk版本，Android Studio 那里查看sdk的界面就能看到。
 
 G:\flutterxx\demo\android\app\build.gradle
 下的配置
---
+++ demo/7、http-web-json/main.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  var _ipAddress = 'Unknown';
+
+  _getIPAddress() async {
+    var url = 'https://httpbin.org/ip';
+    var httpClient = HttpClient();
+
+    String result;
+    try {
+      var request = await httpClient.getUrl(Uri.parse(url));
+      var response = await request.close();
+      if (response.statusCode == HttpStatus.ok) {
+        var json = await response.transform(utf8.decoder).join();
+        print(json);
+        var data = jsonDecode(json);
+        result = data['origin'];
+      } else {
+        result = 'Error getting IP address:\nHttp status ${response.statusCode}';
+      }
+    } catch (exception) {
+      result = 'Failed getting IP address';
+    }
+
+    // If the widget was removed from the tree while the message was in flight,
+    // we want to discard the reply rather than calling setState to update our
+    // non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _ipAddress = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var spacer = const SizedBox(height: 32.0);
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Your current IP address is:'),
+            Text('$_ipAddress.'),
+            spacer,
+            ElevatedButton(
+              onPressed: _getIPAddress,
+              child: const Text('Get IP address'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
